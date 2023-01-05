ca39b9d6e6cd9f59971de88188444bc2cf152fbc
Esteban <esteban_10_02@hotmail.com>
2023-01-05 13:25:31 -0500

    Promises

--- src/components/ItemListContainer/Item.jsx
+++ src/components/ItemListContainer/Item.jsx
@@ -1,16 +1,15 @@
 import ItemCount from '../ItemCount/ItemCount'
-import './card.css'
+import './item.css'
 
-export default function Card(props){
+export default function Item(props){
     const {title, model, year, image, price} = props.item
-    console.log(props.item);
     return(
         <div className="cardProductos">
             <h2>{title}</h2>
             <h3>{model}</h3>
             <h4>{year}</h4>
             <img className="img"src={image} alt="producto en venta" />
-            <p>${price}</p>
+            <p>${price} usd</p>
             <ItemCount/>
         </div>
     )
