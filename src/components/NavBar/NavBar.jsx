3d3ad9bfa916f04cfae57690904cf0ea0ff06927
Esteban <esteban_10_02@hotmail.com>
2023-02-01 18:58:56 -0500

    change to nav

--- src/components/NavBar/NavBar.jsx
+++ src/components/NavBar/NavBar.jsx
@@ -9,13 +9,6 @@ export default function NavBar(props) {
     const context = useContext(cartContext);
     context.test();
 
-    function handleSubmit(evt) {
-        console.log(evt);
-        evt.preventDefault();
-        let user = evt.target.user.value
-        props.onLogin(user)
-    } 
-
     return (
         <div className='barra'>
             <nav>
@@ -24,10 +17,6 @@ export default function NavBar(props) {
                     <Link to="/">Todos</Link>
                     <Link to="/category/New">Modernos</Link>
                     <Link to="/category/Old">Antiguos</Link>
-                    <form onSubmit={handleSubmit}>
-                        <input name="user" type="text" />
-                    </form>
-                    <Button onClick={props.onLogOut} className="btn btn-success">Log out</Button>
                     <Link to="/">
                         <CartWidget />
                     </Link>
